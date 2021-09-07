30074c6fb221711fbea5b63e9c913bcd4530b9a7
Pierre Schweiger <schweiger.pierre@gmail.com>
2021-09-07 18:46:29 +0700

    first commit

---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main h-full w-full flex content-center flex-col">
+    <div class="bg-gray-500 bg-opacity-50 my-15">
+      <p class="text-4xl">
+        <carbon-campsite class="inline-block" />
+      </p>
+      <p>
+        The website is temporarily unavailable.
+      </p>
+      <p>
+        <a class="m-3 text-sm btn bg-dark" rel="noreferrer" href="mailto:gordonfernblick@gmail.com" target="_blank">
+          Contact us.
+        </a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.main {
+  background: url('https://res.cloudinary.com/ahoy/image/upload/c_scale,w_1800/v1631014548/fernblick/1.webp');
+  background-size: cover;
+  background-position: center;
+}
+</style>
